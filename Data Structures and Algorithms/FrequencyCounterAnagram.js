ffcf45040940f1ad1c25330aab7ecdacd9978bb2
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-02-02 19:20:25 -0800

    Update FrequencyCounterAnagram.js
    + added solution

--- Data Structures and Algorithms/FrequencyCounterAnagram.js
+++ Data Structures and Algorithms/FrequencyCounterAnagram.js
@@ -1,23 +1,47 @@
-function validAnagram(word1, word2) {
-  if (word1.length !== word2.length) {
+// My attempt
+// function validAnagram(first, second) {
+//   if (first.length !== second.length) {
+//     return false;
+//   }
+
+//   let w1 = {};
+//   let w2 = {};
+//   for (let letter of first) {
+//     w1[letter] = (w1[letter] || 0) + 1;
+//   }
+//   for (let letter of second) {
+//     w2[letter] = (w2[letter] || 0) + 1;
+//   }
+
+//   for (let key in w1) {
+//     if (!(key ** 2 in w2)) {
+//       return false;
+//     }
+//     if (w2[key ** 2] !== w1[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// Correct solution
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
     return false;
   }
 
-  let w1 = {};
-  let w2 = {};
-  for (let letter of word1) {
-    w1[letter] = (w1[letter] || 0) + 1;
-  }
-  for (let letter of word2) {
-    w2[letter] = (w2[letter] || 0) + 1;
-  }
+  const lookup = {};
 
-  for (let key in w1) {
-    if (!(key ** 2 in w2)) {
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    if (!lookup[letter]) {
       return false;
-    }
-    if (w2[key ** 2] !== w1[key]) {
-      return false;
+    } else {
+      lookup[letter] -= 1;
     }
   }
   return true;
